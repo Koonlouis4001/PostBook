d3e89944d692b3863dbad9a3dd021580a7128e47
Koonlouis <Koonlouis4001@hotmail.com>
2024-08-18 14:44:20 +0700

    sticky header with position fixed

--- my-app/src/components/Navbar.js
+++ my-app/src/components/Navbar.js
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <div className="Navbar">
-            <div>Facebook</div>
-            <div>
-                <Link to={'/'} className="Page__Link">Post</Link>
-                <Link to={'/profile/'} className="Page__Link">Profile</Link>
+        <div className="navbar-container">
+            <div className="navbar">
+                <div>Facebook</div>
+                <div>
+                    <Link to={'/'} className="Page__Link">Post</Link>
+                    <Link to={'/profile/'} className="Page__Link">Profile</Link>
+                </div>
             </div>
         </div>
+        
     );
 }
 
