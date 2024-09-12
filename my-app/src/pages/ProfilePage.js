3875a35c9a44b7c883d362ae19241cfb3cd735cd
Koonlouis <Koonlouis4001@hotmail.com>
2024-09-12 18:27:01 +0700

    add profile's posts in profile page

--- my-app/src/pages/ProfilePage.js
+++ my-app/src/pages/ProfilePage.js
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import ApiConnection from '../ApiConnection';
 import { Context } from '../Context';
 import defaultUser from '../image/defaultUser.png'
+import Post from '../components/Post/Post';
 import moment from 'moment';
 
 function ProfilePage() {
@@ -13,6 +14,7 @@ function ProfilePage() {
   const navigate = useNavigate();
   const [profile,setProfile] = useState();
   const [profileImage,setProfileImage] = useState();
+  const [posts,setPost] = useState([]);
 
   const getProfile = async () => {
     let profileData = await apiConnection.getData(`http://localhost:3000/profile/${id}`);
@@ -21,14 +23,20 @@ function ProfilePage() {
       var blobURL = URL.createObjectURL(image);
       setProfileImage(blobURL);
     }
-    console.log(profile);
     setProfile(profileData);
   }
 
+  const refreshPosts = async () => {
+    let postData = await apiConnection.getData(`http://localhost:3000/posts/profile/${id}`);
+    if(postData) {
+      setPost(postData);
+    }
+  }
+
   useEffect(()=> {
-    console.log(profileId);
     if(id !== undefined) {
       getProfile();
+      refreshPosts();
     }
     else if (profileId !== undefined){
       setProfileImage();
@@ -36,24 +44,35 @@ function ProfilePage() {
     }
   },[id])
 
+  useEffect(()=> {
+    console.log(posts,posts?.length);
+  },[posts])
+
   return (
     <div className="app">
-      <h1>PROFILE PAGE</h1>
-      {profile && 
+      <h1 style={{textAlign:"center"}}>PROFILE PAGE</h1>
+        <h2 style={{textAlign:"center"}}>PROFILE</h2>
         <div className='d-flex profile-box'>
           <img className='profile-image' src={profileImage ? profileImage : defaultUser}/>
           <div className='d-flex profile-status'>
             <div>
-              <div>{profile.id}</div>
-              <div>{profile.profileName}</div>
-              <div>{profile.profileStatus}</div>
-              <div>{profile.created && moment(profile.created).format("DD-MM-YYYY HH:mm:ss.SSS")}</div>
-              <div>{profile.modified && moment(profile.modified).format("DD-MM-YYYY HH:mm:ss.SSS")}</div>
+              <div>Profile ID : {profile?.id ? profile?.id : 'Loading'}</div>
+              <div>Profile Name : {profile?.profileName ? profile?.profileName : 'Loading'}</div>
+              <div>Profile Status : {profile?.profileStatus ? profile?.profileStatus : 'Loading'}</div>
+              <div>Profile Created : {profile?.created ? moment(profile?.created).format("DD-MM-YYYY HH:mm:ss.SSS") : 'Loading'}</div>
+              <div>Profile Last Modified : {profile?.modified ? moment(profile?.modified).format("DD-MM-YYYY HH:mm:ss.SSS") : 'Loading'}</div>
             </div>
+          </div>
+        </div>
+        <div>
+          <h2 style={{textAlign:"center"}}>PROFILE'S POST</h2>
+          <div className='post-page'>
+            {posts?.map((post) => (
+              <Post post={post} refreshPosts={refreshPosts} key={post?.id}/>
+            ))}
           </div>
           
         </div>
-      }
     </div>
   );
 }
