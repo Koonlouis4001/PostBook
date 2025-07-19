b011bd669860ffad9e2b4e1709d4efcd04d877c5
Koonlouis <Koonlouis4001@hotmail.com>
2025-07-19 14:53:52 +0700

    add check length for maximum and minimum bytes for password due to bcrypt encoding change max length and min length message for SignInAuthenDto and SignUpAuthenDto

--- my-back/src/authen/dto/sign-in-authen.dto.ts
+++ my-back/src/authen/dto/sign-in-authen.dto.ts
@@ -1,13 +1,17 @@
-import { IsNotEmpty, IsString, Length } from "class-validator";
+import { IsByteLength, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class SignInAuthenDto {
     @IsString()
-    @IsNotEmpty()
-    @Length(8, 72)
+    @IsNotEmpty({ message: 'Username must not be empty.' })
+    @MinLength(8, { message: 'Username must be at least 8 characters.' })
+    @MaxLength(100, { message: 'Username must be no more than 72 characters.' })
+    @IsByteLength(8,72, { message: 'Username must be between 8 and 72 bytes long.' })
     userName: string;
 
     @IsString()
-    @IsNotEmpty()
-    @Length(8, 72)
+    @IsNotEmpty({ message: 'Password must not be empty.' })
+    @MinLength(8, { message: 'Password must be at least 8 characters.' })
+    @MaxLength(100, { message: 'Password must be no more than 72 characters.' })
+    @IsByteLength(8,72, { message: 'Password must be between 8 and 72 bytes long.' })
     password: string;
 }
