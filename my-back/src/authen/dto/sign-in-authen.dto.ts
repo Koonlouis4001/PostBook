3a658091d651aaf72e6f5e63635b536c7778376c
Koonlouis <Koonlouis4001@hotmail.com>
2025-07-19 14:23:45 +0700

    add minimum and maximum length of userName and password

--- my-back/src/authen/dto/sign-in-authen.dto.ts
+++ my-back/src/authen/dto/sign-in-authen.dto.ts
@@ -1,11 +1,13 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class SignInAuthenDto {
     @IsString()
     @IsNotEmpty()
+    @Length(8, 72)
     userName: string;
 
     @IsString()
     @IsNotEmpty()
+    @Length(8, 72)
     password: string;
 }
--- my-back/src/authen/dto/sign-up-authen.dto.ts
+++ my-back/src/authen/dto/sign-up-authen.dto.ts
@@ -1,11 +1,13 @@
-import { IsDateString, IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class SignUpAuthenDto {
     @IsString()
     @IsNotEmpty()
+    @Length(8, 72)
     userName: string;
 
     @IsString()
     @IsNotEmpty()
+    @Length(8, 72)
     password: string;
 }
